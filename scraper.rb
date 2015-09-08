b54a59a9b13b4cdd72f415db39658d9f687407f0
Luke Bacon <luke@equivalentideas.com>
2015-09-08 18:41:29 +1000

    Scrape all the attributes we want for the first member

--- scraper.rb
+++ scraper.rb
@@ -5,7 +5,10 @@ url = 'https://morph.io/documentation/examples/australian_members_of_parliament'
 page = agent.get(url)
 
 member = {
-  name: page.at('.search-filter-results').at('li').at('.title').inner_text.strip
+  name: page.at('.search-filter-results').at('li').at('.title').inner_text.strip,
+  electorate: page.at('.search-filter-results').at('li').search('dd')[0].inner_text,
+  party: page.at('.search-filter-results').at('li').search('dd')[1].inner_text,
+  url: page.at('.search-filter-results').at('li').at('.title a').attr('href')
 }
 
 p member
